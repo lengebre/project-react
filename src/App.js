cd9bae7514f359822df5f3dec24f538419ec750d
Mo Engelbrecht <lengelbre84@gmail.com>
2020-04-12 14:10:17 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Tweet from './Tweet';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const [isRed, setRed]= useState(false);
+  const[count, setCount] = useState(0);
+
+  const increment = () =>{
+    setCount(count + 1);
+    setRed(!isRed);
+  };
+  const[user, setUser] =useState([
+    {name: "Ed", message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, sed?"},
+    {name: "John", message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum animi rerum odit modi itaque quidem nesciunt placeat voluptatibus. "},
+    {name: "Mike", message: "Lorem ipsum dolor sit amet consectetur adipisicing."}
+
+  ])
+
+  return(
+    <div className="app">
+
+    {user.map(user=>(
+      <Tweet name={user.name} message={user.message}/>
+    ))}
+    {/* <button onClick={increment}> Change Color</button>
+    <h1>{count}</h1>
+    <h1 className={isRed ? 'red' : ""}>Toggle Color</h1> */}
     </div>
-  );
-}
+  )
+};
 
 export default App;
---
+++ src/Tweet.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./App.css"
+
+
+function Tweet({name, message}){
+
+    return(
+        <div className="tweet">
+            <h3>{name}</h3>
+            <p>{message}</p>
+        </div>
+    )
+}
+
+export default Tweet;
